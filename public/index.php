f245c29f5bea07a4d9dada5514b28be37e9fe7d5
resboy <resta.setiawan@bpddiy.co.id>
2025-06-23 12:09:57 +0700

    f10

--- public/index.php
+++ public/index.php
@@ -17,18 +17,18 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 if (strpos($path, '/users') === 0) { //POST
-    handleUsers($path, $method);
+    handleUsers($_SERVER['REQUEST_URI'], $method);
 } elseif (strpos($path, '/rooms') === 0) { //GET
     //Get All Rooms             -> [GET]  ../rooms
     //Get Rooms By Id           -> [GET]  ../rooms?id=idroom
-    handleRooms($path, $method);
+    handleRooms($_SERVER['REQUEST_URI'], $method);
 } elseif (strpos($path, '/bookings') === 0) { //GET & POST
     //Create Booking            -> [POST] ../bookings
     //Edit Booking              -> [POST] ../bookings?m=edit&p=idbooking
     //Delete Booking            -> [POST] ../bookings?m=delete&p=idbooking
     //Get All Bookings          -> [GET]  ../bookings?m=get
     //Get Bookings By Userid    -> [GET]  ../bookings?m=get&p=userid
-    handleBookings($path, $method);
+    handleBookings($_SERVER['REQUEST_URI'], $method);
 } else {
     http_response_code(404);
     echo json_encode(["error" => "Endpoint tidak ditemukan."]);
