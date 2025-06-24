74295677d8e3da2ae7528ccda2e91cfc3dbbc49c
resboy <resta.setiawan@bpddiy.co.id>
2025-06-24 20:11:57 +0700

    y4

--- routes/bookings.php
+++ routes/bookings.php
@@ -37,37 +37,17 @@ function handleBookings($path, $method)
         pg_query_params($db, $insert, [$user_id, $room_id, $activity, $start, $end]);
 
         echo json_encode(["rc" => "00", "message" => "Booking berhasil disimpan."]);
-    } elseif ($endpoint == "/bookings" && $mtd == 'edit' && $prm !== null && $method == "POST") { //EDIT BOOKING
-        $id = $prm; //id booking
-        $room_id = $data['room_id'];
-        $activity = $data['activity_name'];
-        $start = $data['start_time'];
-        $end = $data['end_time'];
-
-        $conflict_query = "SELECT * FROM bookings WHERE room_id = $1 AND start_time < $2 AND end_time > $3 AND booking_id != $4";
-        $conflict_res = pg_query_params($db, $conflict_query, [$room_id, $end, $start, $id]);
-
-        if (pg_num_rows($conflict_res) > 0) {
-            http_response_code(409);
-            echo json_encode(["rc" => "409", "message" => "Jadwal bentrok dengan booking lain."]);
-            return;
-        }
-
-        $update = "UPDATE bookings SET room_id=$1, activity_name=$2, start_time=$3, end_time=$4 WHERE booking_id=$7";
-        pg_query_params($db, $update, [$room_id, $activity, $start, $end, $id]);
-
-        echo json_encode(["rc" => "00", "message" => "Booking berhasil diperbarui."]);
     } elseif ($endpoint == "/bookings" && $mtd == 'delete' && $prm !== null && $method == "POST") { //DELETE BOOKING
         $id = $prm; //id booking
 
-        $check = pg_query_params($db, "SELECT * FROM bookings WHERE booking_id = $1", [$id]);
+        $check = pg_query_params($db, "SELECT * FROM bookings WHERE id = $1", [$id]);
         if (pg_num_rows($check) == 0) {
             http_response_code(404);
             echo json_encode(["rc" => "404", "message" => "Booking tidak ditemukan."]);
             return;
         }
 
-        pg_query_params($db, "DELETE FROM bookings WHERE booking_id = $1", [$id]);
+        pg_query_params($db, "DELETE FROM bookings WHERE id = $1", [$id]);
         echo json_encode(["rc" => "00", "message" => "Booking berhasil dibatalkan."]);
     } elseif ($endpoint == "/bookings" && $mtd == 'iget' && $prm !== null && $method == "GET") { //GET BOOKING BY IDBOOKING
         $uid = $prm;
